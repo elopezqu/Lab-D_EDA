c8571be7acdffb784f04576e81a0c50cad4bb0aa
Edson Joel López Quispe <elopezqu@unsa.edu.pe>
2022-05-15 17:17:25 -0500

    Se agrega la función y se llama a la misma

--- lab02/ejercicio1/ArrayInvertido.java
+++ lab02/ejercicio1/ArrayInvertido.java
@@ -6,5 +6,18 @@ public class ArrayInvertido{
 			System.out.print(num[i]+" ");
 		}
 		System.out.println("");
+		num = invertirArray(num);
+		System.out.print("Array invertido: ");
+		for(int i=0;i<num.length;i++){
+			System.out.print(num[i]+" ");
+		}
+		System.out.println("");
+	}
+	public static int[] invertirArray(int[] A){
+		int Ain[] = new int[A.length];
+		for(int i=A.length-1,int f=0;i>=0;i--,f++){
+			Ain[f] = A[i];
+		}
+		return Ain;
 	}
 }
