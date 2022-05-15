9a6a5bb45f2b7d4748cb0d0178bb4d0ad1715eb3
Edson Joel López Quispe <elopezqu@unsa.edu.pe>
2022-05-15 17:03:13 -0500

    Se imprime el arreglo inicial

---
+++ lab02/ejercicio1/ArrayInvertido.java
@@ -0,0 +1,10 @@
+public class ArrayInvertido{
+	public static void main(String[] args){
+		int num[] = {1,2,3,4,5};
+		System.out.println("Array inicial: ");
+		for(int i=0;i<num.length;i++){
+			System.out.print(num[i]+" ");
+		}
+		System.out.println("");
+	}
+}
