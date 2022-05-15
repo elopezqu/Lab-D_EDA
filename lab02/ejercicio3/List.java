e9106ddccee93cd55f3cbe67add10db418201dfd
oliver092377 <omayta@unsa.edu.pe>
2022-05-15 17:30:08 -0500

    función addNode

--- lab02/ejercicio3/List.java
+++ lab02/ejercicio3/List.java
@@ -6,5 +6,18 @@ public class List {
 	public List() {
 		root=null;
 	}
+	public void addNodo(Object obj) {
+		//Si la lista no tiene "root", el nuevo elemento se convierte en el "root"
+		if(root == null)
+			root= new Node(obj);
+		//Caso contrario el nuevo elemento se convierte en el "root", y los demás recorren un espacio
+		else {
+			Node temp = root;
+			Node nuevo= new Node(obj);
+			nuevo.enlazarSiguiente(temp);
+			root=nuevo;
+		}
+		size++;
+	}
 
 }
